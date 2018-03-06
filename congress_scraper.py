22504d963dd2d5aacef18ce819bdb64ed36bf9e8
heath00 <HeathReineke2018@u.northwestern.edu>
2018-03-06 15:04:13 -0600

    works for just introduced right now, need to abstract for all bill statuses

--- congress_scraper.py
+++ congress_scraper.py
@@ -4,6 +4,11 @@ from selenium.common.exceptions import NoSuchElementException
 import time
 import urllib, http.client
 
+class Bill:
+	def __init__(self, name, description, status):
+		self.name = name
+		self. description = description
+		self.status = status
 
 def main():
 	driver = webdriver.Chrome()
@@ -17,15 +22,27 @@ def main():
 						}
 
 	key_status_words = [['Introduced'],
-						['Passed House', 'Passed Senate']
+						['Passed House', 'Passed Senate'],
 						['Failed to Pass Over Veto', 'Vetoed By President']]
+
 	driver.get(urls_of_statuses["Introduced"])
 
-	list_of_all_bills = driver.find_elements_by_xpath("//div[@id='main']/ol/li[@class='expanded']/span[@class='result-heading']/a")
+	list_of_all_bills = driver.find_elements_by_xpath("//div[@id='main']/ol/li[@class='expanded']")
 
 	bills_in_this_status = []
 	for item in list_of_all_bills:
-		if item.find_element_by_xpath("//")
+		if 'Introduced' in item.find_element_by_xpath(".//span[@class='result-item result-tracker']/p[@class='hide_fromsighted']").text:
+			name = item.find_element_by_xpath(".//span[@class='result-heading']/a").text
+			desc = item.find_element_by_xpath(".//span[@class='result-title']").text
+			status = 'Introduced'
+			temp_bill = Bill(name, desc, status)
+			bills_in_this_status.append(temp_bill)
+		
+		
+	
+	print("bills listed under Introduced one website:", len(list_of_all_bills))
+	print("bills actually in Introduced status:", len(bills_in_this_status))
+
 
 
 if __name__ == '__main__':
